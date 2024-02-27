5491c8f9d85b6a46940b570fd8f7eccd33008017
rachel-daniel <racheldaniel328@gmail.com>
2024-02-26 20:50:17 -0700

    Create EDA.R
    added df to file

---
+++ EDA.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+
+# import data from dropbox
+homes_df <- read_csv('https://www.dropbox.com/scl/fi/zmvcst0q4s3p4bqi5ybqy/15_train.csv?rlkey=rb3r4i2uid0dsdg59otloissq&dl=1')
+
+homes_df
