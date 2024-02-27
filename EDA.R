e4e30b6811d99c5472a14fcd4c958eb5704d2de1
Jacob Sargent <95953463+jrsarge@users.noreply.github.com>
2024-02-26 22:02:04 -0700

    Update EDA.R
    description and dependent variable EDA v1

--- EDA.R
+++ EDA.R
@@ -1,4 +1,9 @@
+
+
 library(tidyverse)
+
+options("tibble.width" = Inf) 
+options("tibble.width" = NULL)
 
 # import data from dropbox
 homes_df <- read_csv('https://www.dropbox.com/scl/fi/zmvcst0q4s3p4bqi5ybqy/15_train.csv?rlkey=rb3r4i2uid0dsdg59otloissq&dl=1')
