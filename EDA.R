993a5776f52880c32eedcee3f031d30faef6400f
sydneyjhull <103790869+sydneyjhull@users.noreply.github.com>
2024-02-28 08:52:32 -0700

    Update EDA.R

--- EDA.R
+++ EDA.R
@@ -1,5 +1,3 @@
-
-
 library(tidyverse)
 
 options("tibble.width" = Inf) 
@@ -32,3 +30,23 @@ homes_df %>%
 
 homes_df %>% 
   filter(is.na(price))
+
+unique_localities <- unique(homes_df$LOCALITY)
+
+ggplot(average_price_by_county, aes(x = LOCALITY, y = PRICE)) +
+  geom_bar(stat = "identity", fill = "lightgreen") +
+  labs(x = "locality", y = "Price", title = "Average Price by County") +
+  scale_y_continuous(labels = scales::comma)
+
+ homes_df %>%
+  ggplot(aes(x = propertysqft, y = price, color = locality)) +
+  geom_point(alpha = 0.6) + 
+  geom_smooth(method = "glm", method.args = list(family = "gaussian"), se = FALSE) + 
+  facet_wrap(~locality, scales = 'free') +
+  labs(x = "Property Square Footage", y = "Price", color = "Locality") +  
+  theme_minimal()
+
+ ggplot(homes_df, aes(x = beds, y = price)) +
+   geom_point(alpha = 0.6) +  # Adding transparency to the points
+   labs(x = "Beds", y = "Price", title = "Price vs Beds") +  # Adjusting axis labels and title
+   theme_minimal()
