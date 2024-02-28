a9bd596aa05302b9781c30d4a7bac57693f13aec
Kyle Klintworth <kdklintworth@gmail.com>
2024-02-28 09:48:48 -0700

    Update EDA.R
    kyles_playground

--- EDA.R
+++ EDA.R
@@ -47,6 +47,46 @@ ggplot(average_price_by_county, aes(x = LOCALITY, y = PRICE)) +
   theme_minimal()
 
  ggplot(homes_df, aes(x = beds, y = price)) +
-   geom_point(alpha = 0.6) +  # Adding transparency to the points
-   labs(x = "Beds", y = "Price", title = "Price vs Beds") +  # Adjusting axis labels and title
+   geom_point(alpha = 0.6) +
+   labs(x = "Beds", y = "Price", title = "Price vs Beds") + 
    theme_minimal()
+ 
+ # this removes the common prefix from the broker title column
+ kyles_playground <- homes_df %>%
+   mutate(brokertitle = sub("Brokered by ", "", brokertitle))
+ 
+ #this gets a count of the number of listings per broker
+ kyles_playground %>% 
+   group_by(brokertitle) %>% 
+   summarise(count = n()) %>% 
+   arrange(desc(count))
+ 
+ # this gives a useless graphic because even when faceted by locality there are too many brokers to really analise anything. 
+ #That being said its evident that brokertitle doesn't really affect price as it seems random
+ kyles_playground %>%
+   group_by(brokertitle, sublocality) %>%  
+   filter(n() > 1) %>%
+   ggplot(aes(x = brokertitle, y = price)) +
+   geom_boxplot() +
+   facet_wrap(~ sublocality) +
+   labs(x = "Broker", y = "Price", title = "Property Prices by Broker and sublocality") +
+   theme(legend.position = "none")
+ 
+# gives a point graph of the effect baths has on pric
+ kyles_playground %>%
+   filter(price != max(price)) %>% 
+   ggplot( aes(x = bath, y = price, color = locality)) +
+   geom_point(alpha = 0.6) +
+   labs(x = "Baths", y = "Price", title = "Price vs Baths") + 
+   theme_bw()
+ 
+ # even better graphic of the effect of baths on price. evident that more baths increase price
+ kyles_playground %>% 
+   ggplot(aes(x = bath, y = price, color = locality)) +
+   geom_point(alpha = 0.6) + 
+   geom_smooth(method = "glm", method.args = list(family = "gaussian"), se = FALSE) + 
+   facet_wrap(~locality, scales = 'free') +
+   labs(x = "Number of Baths", y = "Price", color = "Locality") +  
+   theme_bw()
+
+ 
